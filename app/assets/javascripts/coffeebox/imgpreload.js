2ed56c8695eb45dec6758800adec55a4f5b96917
glebtv <glebtv@gmail.com>
2016-06-28 10:44:50 +0300

    update dependencies

--- app/assets/javascripts/coffeebox/imgpreload.js
+++ app/assets/javascripts/coffeebox/imgpreload.js
@@ -1,128 +1,70 @@
-/* v1.5
-Copyright (c) 2009 Dimas Begunoff, http://www.farinspace.com
-https://github.com/farinspace/jquery.imgpreload
-Licensed under the MIT license
-http://en.wikipedia.org/wiki/MIT_License
+/**
+* jquery.imgpreload 1.6.2 <https://github.com/farinspace/jquery.imgpreload>
+* Copyright 2009-2014 Dimas Begunoff <http://farinspace.com>
+* License MIT <http://opensource.org/licenses/MIT>
 */
-
 if ('undefined' != typeof jQuery)
 {
-  (function($){
+	(function($){
+		'use strict';
 
-    // extend jquery (because i love jQuery)
-    $.imgpreload = function (imgs,settings)
-    {
-      settings = $.extend({},$.fn.imgpreload.defaults,(settings instanceof Function)?{all:settings}:settings);
+		// extend jquery (because i love jQuery)
+		$.imgpreload = function (imgs,settings)
+		{
+			settings = $.extend({},$.fn.imgpreload.defaults,(settings instanceof Function)?{all:settings}:settings);
 
-      // use of typeof required
-      // https://developer.mozilla.org/En/Core_JavaScript_1.5_Reference/Operators/Special_Operators/Instanceof_Operator#Description
-      if ('string' == typeof imgs) { imgs = new Array(imgs); }
+			// use of typeof required
+			// https://developer.mozilla.org/En/Core_JavaScript_1.5_Reference/Operators/Special_Operators/Instanceof_Operator#Description
+			if ('string' == typeof imgs) { imgs = [imgs]; }
 
-      var loaded = new Array();
+			var loaded = [];
 
-      $.each(imgs,function(i,elem)
-      {
-        var img = new Image();
+			$.each(imgs,function(i,elem)
+			{
+				var img = new Image();
 
-        var url = elem;
+				var url = elem;
 
-        var img_obj = img;
+				var img_obj = img;
 
-        if ('string' != typeof elem)
-        {
-          url = $(elem).attr('src') || $(elem).css('background-image').replace(/^url\((?:"|')?(.*)(?:'|")?\)$/mg, "$1");
+				if ('string' != typeof elem)
+				{
+					url = $(elem).attr('src') || $(elem).css('background-image').replace(/^url\((?:"|')?(.*)(?:'|")?\)$/mg, "$1");
 
-          img_obj = elem;
-        }
+					img_obj = elem;
+				}
 
-        $(img).bind('load error', function(e)
-        {
-          loaded.push(img_obj);
+				$(img).bind('load error', function(e)
+				{
+					loaded.push(img_obj);
 
-          $.data(img_obj, 'loaded', ('error'==e.type)?false:true);
-          
-          if (settings.each instanceof Function) { settings.each.call(img_obj); }
+					$.data(img_obj, 'loaded', ('error'==e.type)?false:true);
 
-          // http://jsperf.com/length-in-a-variable
-          if (loaded.length>=imgs.length && settings.all instanceof Function) { settings.all.call(loaded); }
+					// http://msdn.microsoft.com/en-us/library/ie/tkcsy6fe(v=vs.94).aspx
+					if (settings.each instanceof Function) { settings.each.call(img_obj, loaded.slice(0)); }
 
-          $(this).unbind('load error');
-        });
+					// http://jsperf.com/length-in-a-variable
+					if (loaded.length>=imgs.length && settings.all instanceof Function) { settings.all.call(loaded); }
 
-        img.src = url;
-      });
-    };
+					$(this).unbind('load error');
+				});
 
-    $.fn.imgpreload = function(settings)
-    {
-      $.imgpreload(this,settings);
+				img.src = url;
+			});
+		};
 
-      return this;
-    };
+		$.fn.imgpreload = function(settings)
+		{
+			$.imgpreload(this,settings);
 
-    $.fn.imgpreload.defaults =
-    {
-      each: null // callback invoked when each image in a group loads
-      , all: null // callback invoked when when the entire group of images has loaded
-    };
+			return this;
+		};
 
-  })(jQuery);
+		$.fn.imgpreload.defaults =
+		{
+			each: null, // callback invoked when each image is loaded
+			all: null // callback invoked when all images have loaded
+		};
+
+	})(jQuery);
 }
-
-/*
-
-  Usage:
-
-  $('#content img').imgpreload(function()
-  {
-    // this = array of dom image objects
-    // callback executes when all images are loaded
-  });
-
-  $('#content img').imgpreload
-  ({
-    each: function()
-    {
-      // this = dom image object
-      // check for success with: $(this).data('loaded')
-      // callback executes when each image loads
-    },
-    all: function()
-    {
-      // this = array of dom image objects
-      // check for success with: $(this[i]).data('loaded')
-      // callback executes when all images are loaded
-    }
-  });
-
-  $.imgpreload('/images/a.gif',function()
-  {
-    // this = array of dom image objects
-    // check for success with: $(this[i]).data('loaded')
-    // callback
-  });
-
-  $.imgpreload(['/images/a.gif','/images/b.gif'],function()
-  {
-    // this = array of dom image objects
-    // check for success with: $(this[i]).data('loaded')
-    // callback executes when all images are loaded
-  });
-
-  $.imgpreload(['/images/a.gif','/images/b.gif'],
-  {
-    each: function()
-    {
-      // this = dom image object
-      // check for success with: $(this).data('loaded')
-      // callback executes on every image load
-    },
-    all: function()
-    {
-      // this = array of dom image objects
-      // check for success with: $(this[i]).data('loaded')
-      // callback executes when all images are loaded
-    }
-  });
-
-*/
